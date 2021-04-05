87ab0288f01819da4ad529ce959660e41f5a2650
Kumar Sankalp <sankalpback@gmail.com>
2021-04-05 16:54:11 +0530

    First day Commit

---
+++ day1-1.py
@@ -0,0 +1,3 @@
+a=input("what's name of the city you grew up in ?\n")
+b=input("what's your pet's name ?\n")
+print("Your Brand name could be " + a + " "  +b  )
---
+++ day1.py
@@ -0,0 +1,15 @@
+#print("Hello"+ " " + "World")
+#print('Enter Your name')
+#val=input('Enter your name : ')
+#print("The length of your name is -  " +str(len(val)))
+
+
+a=input("Enter value of a ")
+b=input("Enter value of b ")
+print("Swapping varibales ==" )
+c=b
+b=a
+a=c
+print(a)
+print(b)
+
